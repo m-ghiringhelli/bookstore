c37ee771faf80e697f13955f6bc436a09eb0a598
m-ghiringhelli <mghiring@gmail.com>
2022-06-14 09:52:59 -0700

    books test written, failing

--- __tests__/books.test.js
+++ __tests__/books.test.js
@@ -7,8 +7,20 @@ describe('book routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  
+  it('book route should return books as json data', async () => {
+    const res = await request(app).get('/books');
+    const expected = books.map(() => {
+      return {
+        id: book.id,
+        title: book.title,
+        released: book.released
+      };
+    });
+    expect(res.body).toEqual(expected);
+  });
 
   afterAll(() => {
     pool.end();
-  })
+  });
 })
